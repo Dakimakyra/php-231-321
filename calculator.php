1c934bb3f66f8dd98a3e95796c10b479540a3a3e
Dakimakyra <vladkashkin97@mail.ru>
2024-03-05 03:23:06 +0300

    calculator

---
+++ calculator.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Калькулятор</title>
+</head>
+<body>
+    <input type="text" id="display" readonly><br><br>
+
+    <button onclick="addToDisplay('7')">7</button>
+    <button onclick="addToDisplay('8')">8</button>
+    <button onclick="addToDisplay('9')">9</button>
+    <button onclick="addToDisplay('+')">+</button><br>
+
+    <button onclick="addToDisplay('4')">4</button>
+    <button onclick="addToDisplay('5')">5</button>
+    <button onclick="addToDisplay('6')">6</button>
+    <button onclick="addToDisplay('-')">-</button><br>
+
+    <button onclick="addToDisplay('1')">1</button>
+    <button onclick="addToDisplay('2')">2</button>
+    <button onclick="addToDisplay('3')">3</button>
+    <button onclick="addToDisplay('*')">*</button><br>
+
+    <button onclick="addToDisplay('(')">(</button>
+    <button onclick="addToDisplay('0')">0</button>
+    <button onclick="addToDisplay(')')">)</button>
+    <button onclick="addToDisplay('/')">/</button><br><br>
+
+    <button onclick="clearDisplay()">Очистить</button>
+    <button onclick="calculate()">Вычислить</button>
+
+    <script>
+        function addToDisplay(value) {
+            document.getElementById('display').value += value;
+        }
+
+        function clearDisplay() {
+            document.getElementById('display').value = '';
+        }
+
+        function calculate() {
+            let expression = document.getElementById('display').value;
+
+            if (!isValidExpression(expression)) {
+                alert('Некорректное выражение');
+                return;
+            }
+
+            fetch('process.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    expression: expression
+                })
+            })
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('display').value = data;
+            });
+        }
+
+        function isValidExpression(expression) {
+            // Проверка на корректность выражения перед отправкой на сервер
+            // Можно добавить свою логику проверки здесь
+            return true;
+        }
+    </script>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,109 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+
+    if (isset($data['expression'])) {
+        $expression = $data['expression'];
+
+        if (!isValidExpression($expression)) {
+            echo 'Некорректное выражение';
+            exit;
+        }
+
+        $result = customCalculate($expression);
+        echo $result;
+    } else {
+        echo 'Параметр expression не найден';
+    }
+}
+
+function customCalculate($expression) {
+    $tokens = tokenizeExpression($expression);
+    $output = [];
+    $stack = [];
+
+    $precedence = [
+        '*' => 2,
+        '/' => 2,
+        '+' => 1,
+        '-' => 1,
+    ];
+
+    foreach ($tokens as $token) {
+        if (is_numeric($token)) {
+            $output[] = $token;
+        } elseif ($token == '(') {
+            array_push($stack, $token);
+        } elseif ($token == ')') {
+            while (($top = array_pop($stack)) != '(') {
+                $output[] = $top;
+            }
+        } else {
+            while (!empty($stack) && $precedence[end($stack)] >= $precedence[$token]) {
+                $output[] = array_pop($stack);
+            }
+            array_push($stack, $token);
+        }
+    }
+
+    while (!empty($stack)) {
+        $output[] = array_pop($stack);
+    }
+
+    $result = [];
+    foreach ($output as $token) {
+        if (is_numeric($token)) {
+            array_push($result, $token);
+        } else {
+            $operand2 = array_pop($result);
+            $operand1 = array_pop($result);
+            if ($token == '+') {
+                array_push($result, $operand1 + $operand2);
+            } elseif ($token == '-') {
+                array_push($result, $operand1 - $operand2);
+            } elseif ($token == '*') {
+                array_push($result, $operand1 * $operand2);
+            } elseif ($token == '/') {
+                if ($operand2 == 0) {
+                    return 'Деление на ноль';
+                } else {
+                    array_push($result, $operand1 / $operand2);
+                }
+            }
+        }
+    }
+
+    return array_pop($result);
+}
+
+function tokenizeExpression($expression) {
+    $expression = str_replace(' ', '', $expression);
+    $expression = str_split($expression);
+    $tokens = [];
+    $currentNumber = '';
+
+    foreach ($expression as $char) {
+        if (is_numeric($char)) {
+            $currentNumber .= $char;
+        } else {
+            if (!empty($currentNumber)) {
+                $tokens[] = $currentNumber;
+                $currentNumber = '';
+            }
+            $tokens[] = $char;
+        }
+    }
+
+    if (!empty($currentNumber)) {
+        $tokens[] = $currentNumber;
+    }
+
+    return $tokens;
+}
+
+function isValidExpression($expression) {
+    // Проверка на корректность выражения перед вычислением
+    // Можно добавить свою логику проверки здесь
+    return true;
+}
+?>
